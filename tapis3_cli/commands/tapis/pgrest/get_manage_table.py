1955306d84507c2ae30a40960f45f98a899fb617
vaughn <vaughn@tacc.utexas.edu>
2021-10-12 12:54:40 -0500

    Tweak to get_manage_table

--- tapis3_cli/commands/tapis/pgrest/get_manage_table.py
+++ tapis3_cli/commands/tapis/pgrest/get_manage_table.py
@@ -5,7 +5,7 @@ from ...mixins import StringIdentifier
 class TablesShow(Oauth2FormatOne, StringIdentifier):
     """Show details for a table (requires table admin role)
     """
-    DISPLAY_FIELDS = ['table_name', 'root_url', 'table_id', 'columns', 'comments']
+    DISPLAY_FIELDS = ['table_name', 'comments', 'root_url', 'table_id', 'columns']
 
     def get_parser(self, prog_name):
         parser = super().get_parser(prog_name)
