1dba0ddf9f29f6fec7708f81bb15b43db57c0e5d
vaughn <vaughn@tacc.utexas.edu>
2021-10-28 14:10:58 -0500

    Added actors execs list

--- tapis3_cli/commands/tapis/actors/__init__.py
+++ tapis3_cli/commands/tapis/actors/__init__.py
@@ -3,6 +3,7 @@ from .actors_list import ActorsList
 from .actors_delete import ActorsDelete
 from .message import ActorsRun, ActorsSubmit
 from .execs_logs import ActorsExecsLogs
+from .execs_list import ActorsExecsList
 from .aliases_create import AliasesCreate
 from .aliases_delete import AliasesDelete
 from .aliases_update import AliasesUpdate
---
+++ tapis3_cli/commands/tapis/actors/execs_list.py
@@ -0,0 +1,42 @@
+import sys
+from tapis3_cli.utils import nrlist, split_string
+
+from ..client import Oauth2FormatMany
+from ...mixins import LimitsArgs, StringIdentifier
+
+__all__ = ['ActorsExecsList']
+
+
+class ActorsExecsList(Oauth2FormatMany, StringIdentifier, LimitsArgs):
+    """List available Executions for an Actor
+    """
+    def get_parser(self, prog_name):
+        parser = super(ActorsExecsList, self).get_parser(prog_name)
+        parser = super().add_identifier(parser,
+                                        name='Actor ID',
+                                        destination='actor_id',
+                                        optional=False)
+        # TODO - reenable this once limit and offset are supported by V3 Abaco
+        # parser = LimitsArgs.extend_parser(self, parser)
+        return parser
+
+    def take_action(self, parsed_args):
+        super(ActorsExecsList, self).take_action(parsed_args)
+        self.load_client(parsed_args)
+        self.config = {}
+        self.config['actor_id'] = parsed_args.actor_id
+        # if self.get_limit(parsed_args) is not None:
+        #     self.config['limit'] = self.get_limit(parsed_args)
+        # if self.get_offset(parsed_args) is not None:
+        #     self.config['offset'] = self.get_offset(parsed_args)
+
+        resp = self.tapis3_client.actors.listExecutions(
+            **self.config).get('executions')
+
+        filt_resp = self.filter_tapis_results(resp, parsed_args)
+        headers = self.headers_from_result(filt_resp)
+        data = []
+        for item in filt_resp:
+            data.append(item.values())
+
+        return (tuple(headers), tuple(data))
